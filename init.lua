a1c130489bd7940f1d918d114f014453672a1a4e
its-manny <manuel@kochgruber.co.uk>
2025-03-07 22:16:34 +0000

    Updated default shell to powershell.exe

--- init.lua
+++ init.lua
@@ -4,6 +4,9 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- Set default shell to powershell
+vim.o.shell = 'powershell.exe'
+
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
